9a79d615c9716d979eb7a6c4c91a6c2303edd296
Jacek Królikowski <nietaki@gmail.com>
2019-05-04 21:04:06 +0100

    Add AWS RDS SSL connection cert

--- config/config.exs
+++ config/config.exs
@@ -5,8 +5,14 @@ config :hoplon_server,
 
 config :hoplon_server, HoplonServer.Repo,
   # it can be overridden using the DATABASE_URL environment variable
-  url: "ecto://YdmO5QsK:HPR8oylnHI1xTYFGc6@localhost:6543/hoplon_server?ssl=false&pool_size=10"
+  url: "ecto://YdmO5QsK:HPR8oylnHI1xTYFGc6@localhost:6543/hoplon_server?ssl=false&pool_size=10",
+  # https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/UsingWithRDS.SSL.html
+  ssl_opts: [
+    cacertfile: "priv/repo/rds-ca-2015-root.pem"
+  ]
 
 if Mix.env() == :test do
   config :hoplon_server, HoplonServer.Repo, pool: Ecto.Adapters.SQL.Sandbox
 end
+
+
