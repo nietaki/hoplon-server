9acdfa5f08814ad4821c296263fcc5c8e43648a3
Jacek Królikowski <nietaki@gmail.com>
2019-05-05 12:39:39 +0100

    See if app dir is available at distillery config time

--- rel/config/config.exs
+++ rel/config/config.exs
@@ -1,7 +1,8 @@
 use Mix.Config
 
 config :hoplon_server,
-  example: "example is #{System.get_env("EXAMPLE")}"
+  example: "example is #{System.get_env("EXAMPLE")}",
+  app_dir: Application.app_dir("priv/")
 
 
 config :hoplon_server, HoplonServer.Repo,
